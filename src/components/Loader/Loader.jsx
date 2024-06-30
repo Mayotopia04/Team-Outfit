e46b982dd141034543583cc4a759eec951eb728f
hunterbugs <79522220+hunterbugs@users.noreply.github.com>
2024-06-30 13:37:12 +0800

    created loader function

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import PropTypes from 'prop-types';
+import Spinner from 'react-spinners/CircleLoader';
+import s from '../Loader/Loader.module.css';
 
-const Loader = () => {
-    return (
-        <div>Loader</div>
-    )
+export default function Loader({ size = 150 }) {
+  return (
+    <div className={s.loader}>
+      <Spinner
+        color="#ff9406"
+        loading
+        size={size}
+        aria-label="Loading Spinner"
+        speedMultiplier={0.7}
+      />
+    </div>
+  );
 }
 
-export default Loader
+Loader.propTypes = {
+  height: PropTypes.string,
+};

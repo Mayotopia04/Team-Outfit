db3c755ca5ada3a9bec820b23a3fc412f577d2d3
arvirabago <rabago.arvilyzadessa@gmail.com>
2024-07-12 21:44:13 +0800

    updated codes for hompage and sidebard

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
 import { getDailyMeals } from 'redux/products/products-operations';
 import { DiaryDateСalendar } from '../';
 import categories from '../../categories.json';
-import styles from './SideBar.module.scss'; // Import SCSS module
+import s from './SideBar.module.css';
 
 const SideBar = () => {
   const { dailyMeals, date } = useSelector(state => state.products);
@@ -20,21 +21,18 @@ const SideBar = () => {
     dispatch(getDailyMeals({ date: date }));
   }, [dispatch, date, dailyMeals]);
 
-  const getCategoryName = i => categories[i];
 
   const dailyCalories = Number(userDailyDiet?.calories).toFixed(1);
   const consumedCalories = dailyMeals
-    ?.reduce((total, meal) => total + meal.calories, 0)
+    ?.reduce((total, meal) => {
+      return total + meal.calories;
+    }, 0)
     ?.toFixed(1);
   const leftCalories =
     (dailyCalories - consumedCalories).toFixed(1) < 0
       ? 0
       : (dailyCalories - consumedCalories).toFixed(1);
   const percent = ((consumedCalories * 100) / dailyCalories).toFixed(2);
-
-  const foodCategories = userDailyDiet?.categories?.map(i => {
-    return getCategoryName(i);
-  });
 
   const showInfo =
     userDailyDiet?.calories &&
@@ -52,6 +50,7 @@ const SideBar = () => {
     !userDailyDiet?.categories;
 
   const handleBtnClick = e => {
+    
     if (e.target.textContent === activeCategory) {
       setProductsShown(false);
       setActiveCategory('');
@@ -59,7 +58,7 @@ const SideBar = () => {
     } else {
       setActiveCategory(e.target.textContent);
       const productsToShow = userDailyDiet?.notAllowedProduct
-        ?.filter(i => getCategoryName(i.category) === e.target.textContent)
+        ?.filter(i => i.category === e.target.textContent)
         .sort((a, b) => a.title.localeCompare(b.title));
       setSpecificProducts(productsToShow);
       setProductsShown(true);
@@ -67,43 +66,43 @@ const SideBar = () => {
   };
 
   return (
-    <div className={styles.sideBar}>
-      <div className={styles.box}>
-        <section className={styles.section}>
-          <h2 className={styles.title}>
+    <div className={s.sideBar}>
+      <div className={s.box}>
+        <section className={s.section}>
+          <h2 className={s.title}>
             Summary for <DiaryDateСalendar location="sidebar" />
           </h2>
           {noInfo && (
             <p>Please fill out the form on the Calculator page to see your personal statistics.</p>
           )}
           {showInfo && (
-            <ul className={styles.list}>
-              <li className={styles.item}>
+            <ul className={s.list}>
+              <li className={s.item}>
                 <p>Left</p>
-                <p>{showDailyInfo ? `${leftCalories} kcal` : '000 kcal'}</p>
+                <p>{showDailyInfo ? leftCalories : '000'} kcal</p>
               </li>
-              <li className={styles.item}>
+              <li className={s.item}>
                 <p>Consumed</p>
-                <p>{showDailyInfo ? `${consumedCalories} kcal` : '000 kcal'}</p>
+                <p>{showDailyInfo ? consumedCalories : '000'} kcal</p>
               </li>
-              <li className={styles.item}>
+              <li className={s.item}>
                 <p>Daily rate</p>
                 <p>{userDailyDiet.calories} kcal</p>
               </li>
-              <li className={styles.item}>
+              <li className={s.item}>
                 <p>n% of normal</p>
-                <p>{showDailyInfo ? `${percent} %` : '0 %'}</p>
+                <p>{showDailyInfo ? percent : 0} %</p>
               </li>
             </ul>
           )}
         </section>
-        <section className={styles.section}>
-          <h2 className={styles.title}>Food not recommended</h2>
+        <section className={s.section}>
+          <h2 className={s.title}>Food not recommended</h2>
           {showInfo && (
-            <ul className={styles.list}>
-              {foodCategories?.map((element, index) => (
-                <li key={`not-recommended-categories-${index}`} className={styles.item}>
-                  <button type="button" onClick={handleBtnClick} className={styles.button}>
+            <ul className={s.list}>
+              {userDailyDiet?.categories?.map((element, index) => (
+                <li key={`not-recommended-categories-${index}`} className={s.item}>
+                  <button type="button" onClick={handleBtnClick} className={s.button}>
                     {element}
                   </button>
                 </li>
@@ -115,18 +114,18 @@ const SideBar = () => {
         </section>
       </div>
       {showInfo && productsShown && (
-        <section className={styles.additional}>
-          <h3 className={styles.title}>Not recommended products in category {activeCategory}</h3>
-          <ul className={styles.productsList}>
-            {specificProducts?.map((element, index) => (
+        <section className={s.additional}>
+          <h3 className={s.title}>Not recommended products in category {activeCategory}</h3>
+          <ul className={s.productsList}>
+            {specificProducts.map((element, index) => (
               <li key={`not-recommended-products-${index}`}>{element.title}</li>
             ))}
           </ul>
         </section>
       )}
+    
     </div>
   );
 };
 
 export default SideBar;
-

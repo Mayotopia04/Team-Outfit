00b96f93ab8aec73a68516385eaf80eddb48de09
Ruby Mateo <fangprincess_2k3@yahoo.com>
2024-07-14 20:17:10 +0800

    from bhie-branch:added background animation and teamlogo on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import s from './Footer.module.css';
+import 'animate.css';
+import teamLogo from '../../assets/png/teamOutfit_Logo.png';
 
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return (
+    <footer className={s.footer}>
+      <div className={s.footerContent}>
+        <p>
+          Copyright © {new Date().getFullYear()} GoIT. Created by{' '}
+          <Link className={s.link + ' animate__shakeY'} to={'/developers'}>
+            Team <img className={s.teamLogo} src={teamLogo} alt="team logo" />
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
